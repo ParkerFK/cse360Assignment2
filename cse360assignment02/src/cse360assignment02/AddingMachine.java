0c83b73fed91db9f2111eb858c73e67e5e984925
parke <parke@ParkersLaptop>
2020-10-07 14:05:15 -0700

    Initial commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,38 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /*
+   * Returns the value of variable total. 
+   * @return the value of total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  /*
+   * Adds a value to the variable total.
+   * @param value the value to be added to total.
+   */
+  public void add (int value) {
+  }
+  /*
+   * Subtracts a value to the variable total.
+   * @param value the value to be subtracted total.
+   */
+  public void subtract (int value) {
+  }
+  /*
+   * Returns the value of total as a String.
+   * @return total as a String
+   */
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
